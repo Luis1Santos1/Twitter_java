1ab7907664756d0484693a196f3d7d948759e775
Allan Ferreira <Allan@TCLs-MacBook-Pro.local>
2023-04-15 12:37:17 -0300

    dia 14/04 aula de sexta

--- Twitter.java
+++ Twitter.java
@@ -7,16 +7,16 @@ public class Twitter {
     public static ArrayList<Usuario> usuarios = new ArrayList<>();
     public static Usuario usuarioLogado = null;
 
-    //Cria o usuário dentro de um array
+    // Cria o usuário dentro de um array
     public static Usuario criaUsuario() {
         System.out.print("Crie o seu nome: ");
-        String nome = leitor.next();
+        String nome = leitor.nextLine();
         System.out.print("Crie seu login: ");
-        String login = leitor.next();
+        String login = leitor.nextLine();
         System.out.print("Crie seu email: ");
-        String email = leitor.next();
+        String email = leitor.nextLine();
         System.out.print("Crie sua senha: ");
-        String senha = leitor.next();
+        String senha = leitor.nextLine();
 
         Usuario novoUsuario = new Usuario(nome, login, email, senha);
 
@@ -24,73 +24,76 @@ public class Twitter {
         return novoUsuario;
     }
 
-    //lista usuarios cadastrados
+    // lista usuarios cadastrados
     public static void listaUsuarios() {
-        for(Usuario usuario : usuarios) {
+        for (Usuario usuario : usuarios) {
             System.out.println("Usuario: " + usuario.getLogin());
         }
     }
 
-    //loga em um usuario ja cadastrado
-    public static void logUsuario(ArrayList<Usuario> usuarios){
+    // loga em um usuario ja cadastrado
+    public static void logUsuario(ArrayList<Usuario> usuarios) {
         System.out.print("Digite seu login para logar: \n");
         String login = leitor.next();
         System.out.print("Digite sua senha para logar: \n");
         String senha = leitor.next();
-        
 
-        for(Usuario usuario : usuarios) {
-            if(usuario.getLogin().equals(login) && usuario.getSenha().equals(senha)) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getLogin().equals(login) && usuario.getSenha().equals(senha)) {
                 usuarioLogado = usuario;
                 usuario.setLogado(true);
                 System.out.println("Usuário logado com sucesso!");
                 System.out.println("Usuario logado: " + usuario.getLogin());
                 return;
-            }
-            else{
+            } else {
                 System.out.println("Usuário incorreto");
             }
-        } 
+        }
     }
 
     public static void deslogUsuario(ArrayList<Usuario> usuarios) {
         System.out.print("Digite seu login");
         String login = leitor.next();
 
-        for(Usuario usuario : usuarios) {
-            if(usuario.getLogin().equals(login)) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getLogin().equals(login)) {
                 System.out.println("Usuário deslogado!");
-                
-            }
-            else{
+
+            } else {
                 System.out.println("Usuário não está logado");
             }
-        } 
+        }
 
     }
-   public static void main(String[] args) {
-        int digito = 0;
-            while(digito != 3){
-                System.out.println("Para Criar Usuario digite 1, \n Para Listar usuários digite 2");
-                digito = leitor.nextInt();
 
-                if(digito == 1){
-                    criaUsuario();
-                }
-                else if(digito == 2){
-                    listaUsuarios();
-                }
-                else if(digito == 3){
-                    logUsuario(usuarios);
-                }
-                else if(digito == 4){
-                    logUsuario(usuarios);
-                }
+    public static void main(String[] args) {
+        String text = "";
+        
+        while(!text.equalsIgnoreCase("logar usuario")){
+            System.out.println("-- Cadastrar Usuário\n-- Listar Usuários\n-- Logar Usuário\n-- Deslogar Usuário\n-- Tweetar\n-- Mostrar Tweets do Feed\n-- Remover Tweet de um Usuário\n-- Alterar Senha de um Usuário\n-- Remover um Usuário\n-- Imprimir Estatísticas\n-- Finalizar Programa");
+            text = leitor.nextLine();
 
-            }
 
-    
+            if (text.equalsIgnoreCase("Cadastrar Usuario")) {
+                criaUsuario();
+            } else if (text.equalsIgnoreCase("Listar usuarios")) {
+                listaUsuarios();
+            } else if (text.equalsIgnoreCase("Logar usuario")) {
+                logUsuario(usuarios);
+            } else if (text.equalsIgnoreCase("Mostrar Tweets do Feed")) {
 
+            } else if (text.equalsIgnoreCase("Imprimir Estatísticas")) {
+
+            } else if (text.equalsIgnoreCase("Finalizar Programa")) {
+                System.out.println("Encerrando o programa...");
+                break;
+            } 
+        }
+
+        
+        while(usuarioLogado.logado == true){
+            
+        }
+        
     }
-    
 }
